688ec61ab82f76caa6ca6c33c94ca389dd31da03
alireza <oveisalireza@gmail.com>
2023-08-05 22:28:14 +0330

    Update rock,paper.game.py

--- rock,paper.game.py
+++ rock,paper.game.py
@@ -5,9 +5,9 @@ b=0
 computer=0
 user=0
 while a =='yes':
-    item=['paper','rock','scissor']
+    item=['paper','rock','scissors']
     computer_choice=random.choice(item)
-    user_choice=input('please select your item: (paper , rock , scissor) ')
+    user_choice=input('please select your item: (paper , rock , scissors) ')
     print(f'computer choice :  {computer_choice}')
     if user_choice==computer_choice:
         print(f'tie ')
@@ -15,7 +15,7 @@ while a =='yes':
         user=user+1
         computer=computer+1
     elif user_choice=='paper':
-        if computer_choice=='scissor':
+        if computer_choice=='scissors':
             print(f'computer win')
             print('-'*20)
             computer=computer+1
@@ -32,7 +32,7 @@ while a =='yes':
             print('you win')
             print('-'*20)
             user=user+1
-    elif user_choice=='scissor':
+    elif user_choice=='scissors':
         if computer_choice=='rock':
             print('computer win')
             print('-'*20)
@@ -46,6 +46,7 @@ while a =='yes':
         exit()
     b=b+1
     print(f'number you played : {b}')
+    print('-'*20)
     print(f'computer score : {computer}')
     print(f'your score : {user}')
     a=input('do you want to play again? ')
